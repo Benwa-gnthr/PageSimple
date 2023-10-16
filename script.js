9031c89e439edd74a64ff5f2f6998954b8b84400
Benwa <bgonthier74@gmail.com>
2023-10-16 16:40:10 +0200

    création du week-end finaliser aujourd'hui

---
+++ script.js
@@ -0,0 +1,5 @@
+var bouton = document.getElementById('monBouton');
+
+bouton.addEventListener('click', function(){
+    alert("Bonjour, ceci est un simple message pour vous dire que vous êtes une magnifique personne.")
+});
